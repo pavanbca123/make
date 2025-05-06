ceac304137bd55d421e9eef62be4f2a3409156a8
pavanbca123 <patilsanchita025@gmail.com>
2025-05-06 14:37:53 +0530

    save

---
+++ Employee.java
@@ -0,0 +1,24 @@
+package FDb.Add;
+
+public class Employee {
+    String name;
+    int id;
+    double salary;
+
+    public Employee(String name,int id, double salary){
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+    }
+    public void displayInfo(){
+       System.out.println("Employee id:"+id);
+       System.out.println("Employee Name:"+name);
+       System.out.println("Employee salary:"+salary);
+
+    }
+    public static void main(String[] args) {
+        Employee emp1=new Employee("xyz",101,50000.0);
+        emp1.displayInfo();
+    }
+    
+}
